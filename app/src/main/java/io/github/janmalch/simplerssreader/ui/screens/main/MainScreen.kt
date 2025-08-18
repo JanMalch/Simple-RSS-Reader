14ff3975bc2f29c3f900fcaa7ebb1ff1c7fbfe5b
JanMalch <25508038+JanMalch@users.noreply.github.com>
2025-08-18 19:06:27 +0200

    fix: fix scroll behaviour of feed
    Reverts c43d4372b616af30074de5c09d437c05728c9c80.

--- app/src/main/java/io/github/janmalch/simplerssreader/ui/screens/main/MainScreen.kt
+++ app/src/main/java/io/github/janmalch/simplerssreader/ui/screens/main/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DoneAll
@@ -35,6 +36,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,6 +56,7 @@ import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import coil3.compose.AsyncImage
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import io.github.janmalch.shed.Shed
@@ -175,7 +178,12 @@ fun MainScreen(
                 .fillMaxSize()
                 .padding(innerPadding),
         ) {
+            val lazyListState = rememberLazyListState()
+            LaunchedEffect(pagingItems.itemCount) {
+                lazyListState.requestScrollToItem(0)
+            }
             LazyColumn(
+                state = lazyListState,
                 modifier = Modifier.fillMaxSize()
             ) {
                 if (pagingItems.itemCount == 0 && pagingItems.loadState.isIdle) {
@@ -204,8 +212,7 @@ fun MainScreen(
                 }
                 items(
                     count = pagingItems.itemCount,
-                    // not specifying a key, so that scroll position doesn't
-                    // stick to an item, when items change
+                    key = pagingItems.itemKey { it.id.id },
                     contentType = pagingItems.itemContentType { "FeedItem" },
                 ) { index ->
                     val item = pagingItems[index] ?: return@items
@@ -241,9 +248,7 @@ fun MainScreen(
                                     .orEmpty()
                             )
                         },
-                        modifier = Modifier
-                            .animateItem()
-                            .clickable { onItemClick(item.id, isOnlyUnreadVisible) }
+                        modifier = Modifier.clickable { onItemClick(item.id, isOnlyUnreadVisible) }
                     )
                     HorizontalDivider()
                 }
