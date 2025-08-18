cf6a4ac837a034e489f34fa88c294a0b2fb72c16
JanMalch <25508038+JanMalch@users.noreply.github.com>
2025-08-18 11:56:31 +0200

    fix: ensure content is scrollable

--- app/src/main/java/io/github/janmalch/simplerssreader/ui/screens/reader/ReaderScreen.kt
+++ app/src/main/java/io/github/janmalch/simplerssreader/ui/screens/reader/ReaderScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.automirrored.outlined.OpenInNew
@@ -320,6 +322,7 @@ private fun ItemPager(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(MaterialTheme.colorScheme.surface)
+                    .verticalScroll(rememberScrollState())
                     .padding(contentPadding),
             ) {
                 Text(
